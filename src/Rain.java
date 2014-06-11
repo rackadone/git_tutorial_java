48b822c27bace47070123dce5a13cc500ff6fa71
rackadone <rackadone@gmail.com>
2014-06-11 18:26:12 -0500

    Paul: Created Rain.java

---
+++ src/Rain.java
@@ -0,0 +1,12 @@
+
+public class Rain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It's raining outside and I'm wet.");
+	}
+
+}
